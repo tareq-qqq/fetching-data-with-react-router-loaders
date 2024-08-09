6cf655f5435c2ed6e7796b8cfa429bf5a5aa946b
x287x <115352423+x287x@users.noreply.github.com>
2024-08-10 00:04:58 +0300

    Initiate queryClient in App and add options

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorElement from "./components/errors/error-element";
@@ -8,7 +8,10 @@ import Post from "./routes/post";
 import Posts from "./routes/posts";
 import Root from "./routes/root";
 import User, { loader as userLoader } from "./routes/user";
-import queryClient from "./vendors/query-client";
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 
 const router = createBrowserRouter([
   {
